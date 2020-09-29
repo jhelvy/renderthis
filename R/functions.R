018e5eb5977397c95d1e73dbc63975934f5b15c4
John Helveston <john.helveston@gmail.com>
2020-09-29 06:29:33 -0400

    working on build_gif

--- R/functions.R
+++ R/functions.R
@@ -3,10 +3,29 @@
 #' @param output_file The name of the path to the saved html file.
 #' @export
 build_html <- function(input, output_file = NULL) {
+    if (! file.exists(input)) {
+        return(NULL)
+    }
     rmarkdown::render(
         input = input,
         output_file = output_file,
         output_format = 'xaringan::moon_reader')
+}
+
+#' Creates a html of the xaringan Rmd file.
+#' @param input The Rmd file to convert to a html.
+build_temp_html <- function(input, folder = NULL) {
+    if (is.null(folder)) {
+        folder <- tempdir()
+    }
+    # Copy the Rmd file to temp folder
+    rmdPath <- file.path(folder, 'temp.Rmd')
+    file.copy(from = input, to = rmdPath)
+    # Build html as temp file
+    html <- rmarkdown::render(
+        input = rmdPath,
+        output_file =  file.path(folder, 'temp.html'))
+    return(html)
 }
 
 #' Creates a PDF of the xaringan Rmd file or html file.
@@ -17,10 +36,13 @@ build_pdf <- function(input, output_file = NULL) {
     if (! file.exists(input)) {
         return(NULL)
     }
-    if (grepl(".Rmd", input)) {
+    paths <- DescTools::SplitPath(input)
+    if (paths$extension == "Rmd") {
         pdf <- build_temp_pdf(input)
-        root <- getPaths(input)
-        file.copy(from = pdf, to = )
+        filename <- paste0(paths$fullpath, paths$filename, ".pdf")
+        file.copy(from = pdf, to = filename)
+    } else if (is.null(output_file)) {
+        pagedown::chrome_print(input = input)
     } else {
         pagedown::chrome_print(
             input  = input,
@@ -28,24 +50,11 @@ build_pdf <- function(input, output_file = NULL) {
     }
 }
 
-getPaths <- function(x) {
-    if (dirname(x) == x) {
-        return(x)
-    }
-    return(c(basename(x), split_path(dirname(x))))
-}
-
 #' Creates a PDF of the xaringan Rmd file.
 #' @param input The Rmd file to convert to a PDF.
 build_temp_pdf <- function(input) {
-    # Create a temporary directory and copy the Rmd file there
     folder <- tempdir()
-    rmdPath <- file.path(folder, 'temp.Rmd')
-    file.copy(from = input, to = rmdPath)
-    # Build html as temp file, then build pdf as temp file
-    html <- rmarkdown::render(
-        input = rmdPath,
-        output_file =  file.path(folder, 'temp.html'))
+    html <- build_temp_html(input, folder)
     pdf <- pagedown::chrome_print(
         input  = html,
         output = file.path(folder, 'temp.pdf'))
@@ -58,31 +67,66 @@ build_temp_pdf <- function(input) {
 #' @param output_file The name of the path to the saved PNG file.
 #' @export
 build_thumbnail <- function(input, output_file = NULL) {
-    if (grepl(".Rmd", input)) {
-        input <- rmarkdown::render(
-            input = input,
-            output_file = tempfile(fileext = '.html'),
-            output_format = 'xaringan::moon_reader')
+    if (! file.exists(input)) {
+        return(NULL)
     }
-    pagedown::chrome_print(
+    paths <- DescTools::SplitPath(input)
+    if (paths$extension == "Rmd") {
+        thumbnail <- build_temp_thumbnail(input)
+        filename <- paste0(paths$fullpath, paths$filename, ".png")
+        file.copy(from = html, to = filename)
+    } else if (is.null(output_file)) {
+        pagedown::chrome_print(input = input, format = "png")
+    } else {
+        pagedown::chrome_print(
+            input  = input,
+            output = output_file,
+            format = "png")
+    }
+}
+
+#' Creates a html of the xaringan Rmd file.
+#' @param input The Rmd file to compile to a html file.
+build_temp_thumbnail <- function(input) {
+    folder <- tempdir()
+    html <- build_temp_html(input, folder)
+    thumbnail <- pagedown::chrome_print(
         input  = input,
-        output = output_file,
+        output = file.path(folder, 'temp.png'),
         format = "png")
+    return(thumbnail)
 }
 
 #' Creates a GIF image of the xaringan Rmd, html, or PDF file.
 #' or html file.
-#' @param input The Rmd, html, or PDF file.
+#' @param input Path to the Rmd, html, or PDF file.
 #' @param output_file The name of the path to the saved GIF file.
 #' @param density Resolution of the resulting GIF file.
 #' @export
 build_gif <- function(input, output_file = NULL, density = "72x72") {
-    if (grepl(".Rmd", input) | grepl(".html", input)) {
-        input <- xaringanBuilder::build_pdf(
-            input = input,
-            output_file = tempfile(fileext = '.html'))
+    if (! file.exists(input)) {
+        return(NULL)
+    }
+    paths <- DescTools::SplitPath(input)
+    if (paths$extension == "Rmd") {
+        pdf <- build_temp_pdf(input)
+    } else if (paths$extension == "html") {
+        pdf <- build_temp_pdf(input)
     }
     pdf <- magick::image_read(input, density = density)
+    build_gif_from_pdf(input, output_file, density)
+}
+
+#' Creates a GIF image of the xaringan PDF file.
+#' or html file.
+#' @param pdf The PDF file to convert to a GIF.
+#' @param output_file The name of the path to the saved GIF file.
+#' @param density Resolution of the resulting GIF file.
+#' @export
+build_gif_from_pdf <- function(pdf, output_file = NULL, density = "72x72") {
+    if (! file.exists(input)) {
+        return(NULL)
+    }
     pngs <- list()
     for (i in 1:length(pdf)) {
         pngs[[i]] <- magick::image_convert(pdf[i], 'png')
--- test/build.R
+++ test/build.R
@@ -6,3 +6,9 @@ build_pdf(here::here("test", "demo.Rmd"))
 
 # Build PDF from html file
 build_pdf(here::here("test", "demo.html"))
+
+# Build first slide thumbnail from Rmd file
+build_thumbnail(here::here("test", "demo.Rmd"))
+
+# Build first slide thumbnail from html file
+build_thumbnail(here::here("test", "demo.html"))
