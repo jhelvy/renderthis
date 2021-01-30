a9f78b1cd0145568c79caf4394cf415be7d6e5cb
John Helveston <john.helveston@gmail.com>
2021-01-30 16:14:25 -0500

    added DescTools dependency

--- inst/example/example.R
+++ inst/example/example.R
@@ -2,21 +2,23 @@
 # remotes::install_github('jhelvy/xaringanBuilder')
 library(xaringanBuilder)
 
+setwd(here::here("inst", "example"))
+
 # Build html from Rmd file
-build_html(here::here("example", "slides.Rmd"))
+build_html("slides.Rmd")
 
 # Build pdf from Rmd or html file
-build_pdf(here::here("example", "slides.Rmd"))
-build_pdf(here::here("example", "slides.html"))
+build_pdf("slides.Rmd")
+build_pdf("slides.html")
 
 # Build gif from Rmd, html, or pdf file
-build_gif(here::here("example", "slides.Rmd"))
-build_gif(here::here("example", "slides.html"))
-build_gif(here::here("example", "slides.pdf"))
+build_gif("slides.Rmd")
+build_gif("slides.html")
+build_gif("slides.pdf")
 
 # Build first slide thumbnail from Rmd or html file
-build_thumbnail(here::here("example", "slides.Rmd"))
-build_thumbnail(here::here("example", "slides.html"))
+build_thumbnail("slides.Rmd")
+build_thumbnail("slides.html")
 
 # Build html, pdf, gif, and thumbnail of first slide from Rmd file
-build_all(here::here("example", "slides.Rmd"))
+build_all("slides.Rmd")
