3f36b5088457327b4e8b1ac9e07e4697d0b2cf2d
Garrick Aden-Buie <garrick@adenbuie.com>
2021-07-20 10:21:47 -0400

    Update `build_mp4()`

--- R/mp4.R
+++ R/mp4.R
@@ -3,27 +3,28 @@
 #' Build xaringan slides as a mp4 video file. The function builds to the pdf,
 #' converts each slide in the pdf to a png, and then converts the deck of
 #' png files to a mp4 video file.
-#' @param input Path to a Rmd file, html file, pdf file, or a url.
-#' If the input is a url to xaringan slides on a website, you must provide the
-#' full url ending in ".html".
+#'
+#' @param input Path to a Rmd file, html file, pdf file, or a url. If the input
+#'   is a url to xaringan slides on a website, you must provide the full url
+#'   ending in ".html".
 #' @param output_file Name of the output mp4 file.
-#' @param density Resolution of the resulting pngs in each slide file.
-#' Defaults to `100`.
-#' @param slides A vector of the slide number(s) to include in the mp4.
-#' Defaults to `NULL`, in which case all slides are included.
+#' @param density Resolution of the resulting pngs in each slide file. Defaults
+#'   to `100`.
+#' @param slides A vector of the slide number(s) to include in the mp4. Defaults
+#'   to `NULL`, in which case all slides are included.
 #' @param fps Frames per second of the resulting mp4 file.
 #' @param complex_slides For "complex" slides (e.g. slides with panelsets or
-#' other html widgets or advanced features), set `complex_slides = TRUE`.
-#' Defaults to `FALSE`. This will use the {chromote} package to iterate through
-#' the slides at a pace set by the `delay` argument. Requires a local
-#' installation of Chrome.
-#' @param partial_slides Should partial (continuation) slides be
-#' included in the output? If `FALSE`, the default, only the complete slide
-#' is included in the PDF.
-#' @param delay Seconds of delay between advancing to and printing
-#' a new slide. Only used if `complex_slides = TRUE` or `partial_slides =
-#' TRUE`.
-#' @export
+#'   other html widgets or advanced features), set `complex_slides = TRUE`.
+#'   Defaults to `FALSE`. This will use the {chromote} package to iterate
+#'   through the slides at a pace set by the `delay` argument. Requires a local
+#'   installation of Chrome.
+#' @param partial_slides Should partial (continuation) slides be included in the
+#'   output? If `FALSE`, the default, only the complete slide is included in the
+#'   PDF.
+#' @param delay Seconds of delay between advancing to and printing a new slide.
+#'   Only used if `complex_slides = TRUE` or `partial_slides = TRUE`.
+#' @inheritParams build_png
+#'
 #' @examples
 #' \dontrun{
 #' # Build mp4 from Rmd, html, pdf, or url
@@ -32,6 +33,8 @@
 #' build_mp4("slides.pdf")
 #' build_mp4("https://jhelvy.github.io/xaringanBuilder/reference/figures/slides.html")
 #' }
+#'
+#' @export
 build_mp4 <- function(
     input,
     output_file = NULL,
@@ -40,40 +43,42 @@ build_mp4 <- function(
     fps = 1,
     complex_slides = FALSE,
     partial_slides = FALSE,
-    delay = 1
+    delay = 1,
+    keep_intermediates = FALSE
 ) {
+    if (is.null(output_file)) {
+        output_file <- path_from(input, "mp4")
+    }
+
     # Check input and output files have correct extensions
     assert_path_ext(input, c("rmd", "html", "pdf"))
     assert_path_ext(output_file, "mp4")
 
-    # Build input and output paths
-    paths <- build_paths(input, output_file)
-
     # Build html and / or pdf (if input is not pdf)
+    step_pdf <- input
     if (!test_path_ext(input, "pdf")) {
-        build_to_pdf(input, paths, complex_slides, partial_slides, delay)
-        paths <- build_paths(input = paths$output$pdf, output_file)
+        step_pdf <- path_from(output_file, "pdf", temporary = !keep_intermediates)
+        build_pdf(
+            input,
+            step_pdf,
+            complex_slides = complex_slides,
+            partial_slides = partial_slides,
+            delay = delay,
+            keep_intermediates = keep_intermediates
+        )
     }
 
     # Build mp4 from pdf
-    input <- paths$input$pdf
-    output_file <- paths$output$mp4
-    proc <- cli_build_start(input, output_file)
-    pngs <- pdf_to_imgs(input, density)
+    proc <- cli_build_start(step_pdf, output_file)
+    imgs <- pdf_to_imgs(step_pdf, density)
+    slides <- slides_arg_validate(slides, imgs)
 
-    # Keep only selected slides by number
-    if (!is.null(slides)) {
-        pngs <- pngs[slides]
-    }
+    temp_folder <- withr::local_tempdir()
+    png_root <- path_from(output_file, "png", dir = temp_folder)
+    png_paths <- append_to_file_path(png_root, paste0("_", slides))
 
-    temp_folder <- tempdir()
-    png_root <- fs::path_ext_set(fs::path_file(output_file), "png")
-    png_paths <- c()
-    for (i in seq(length(pngs))) {
-        png_name <- append_to_file_path(png_root, paste0("_", i))
-        png_path <- magick::image_write(
-            pngs[i], fs::path(temp_folder, png_name))
-        png_paths <- c(png_paths, png_path)
+    for (i in seq_along(slides)) {
+        magick::image_write(imgs[slides[i]], png_paths[i])
     }
 
     res <- av::av_encode_video(
---
+++ tests/testthat/test-mp4.R
@@ -0,0 +1,45 @@
+test_that("build_gif() builds an mp4", {
+    skip_if_not_installed("av")
+
+    tmpdir <- withr::local_tempdir()
+    fs::file_copy(test_path("slides", "basic.pdf"), tmpdir, overwrite = TRUE)
+
+    withr::local_dir(tmpdir)
+
+    quiet_cli(
+        build_mp4("basic.pdf", "basic.mp4", fps = 0.5, slides = 1:2)
+    )
+
+    expect_true(fs::file_exists("basic.mp4"))
+    expect_length(fs::dir_ls(regexp = "png$"), 0)
+
+    mp4_info <- av::av_video_info("basic.mp4")
+    expect_equal(mp4_info$video$framerate, 0.5)
+    expect_equal(mp4_info$video$width / mp4_info$video$height, 4/3, tolerance = 0.01)
+
+    frames <- magick::image_info(magick::image_read_video("basic.mp4", fps = 0.5))
+    expect_equal(nrow(frames), 2)
+})
+
+test_that("build_gif() builds a widescreen mp4", {
+    skip_if_not_installed("av")
+
+    tmpdir <- withr::local_tempdir()
+    fs::file_copy(test_path("slides", "basic-wide.pdf"), tmpdir, overwrite = TRUE)
+
+    withr::local_dir(tmpdir)
+
+    quiet_cli(
+        build_mp4("basic-wide.pdf")
+    )
+
+    expect_true(fs::file_exists("basic-wide.mp4"))
+    expect_length(fs::dir_ls(regexp = "png$"), 0)
+
+    mp4_info <- av::av_video_info("basic-wide.mp4")
+    expect_equal(mp4_info$video$framerate, 1)
+    expect_equal(mp4_info$video$width / mp4_info$video$height, 16/9, tolerance = 0.01)
+
+    frames <- magick::image_info(magick::image_read_video("basic-wide.mp4", fps = 1))
+    expect_equal(nrow(frames), 3)
+})
