3ae4bf8fd9e2460af66a92b1f859b1409e7f9590
Garrick Aden-Buie <garrick@adenbuie.com>
2021-07-20 22:04:35 -0400

    Guard `build_pptx()` tests from missing officer or chrome

--- tests/testthat/test-pptx.R
+++ tests/testthat/test-pptx.R
@@ -1,4 +1,30 @@
-test_that("build_pptx() simple", {
+test_that("build_pptx() simple from pdf", {
+    skip_if_not_installed("officer")
+
+    tmpdir <- withr::local_tempdir()
+    fs::file_copy(test_path("slides", "basic.pdf"), tmpdir, overwrite = TRUE)
+
+    withr::local_dir(tmpdir)
+
+    quiet_cli(
+        build_pptx("basic.pdf", "slides.pptx")
+    )
+
+    expect_true(fs::file_exists("slides.pptx"))
+
+    expect_equal(
+        nrow(officer::pptx_summary(officer::read_pptx("slides.pptx"))),
+        3 # three slides
+    )
+
+    slide_size <- officer::slide_size(officer::read_pptx("slides.pptx"))
+    expect_equal(slide_size$width / slide_size$height, 4/3)
+})
+
+test_that("build_pptx() simple from Rmd", {
+    skip_if_not_installed("officer")
+    skip_if_not_chrome_installed()
+
     tmpdir <- withr::local_tempdir()
     fs::dir_copy(test_path("slides", "basic"), tmpdir, overwrite = TRUE)
 
@@ -21,31 +47,26 @@ test_that("build_pptx() simple", {
     expect_equal(slide_size$width / slide_size$height, 4/3)
 })
 
-test_that("build_pptx() wide-angle", {
+
+test_that("build_pptx() widescreen", {
+    skip_if_not_installed("officer")
+
     tmpdir <- withr::local_tempdir()
-    fs::dir_copy(test_path("slides", "basic"), tmpdir, overwrite = TRUE)
+    fs::file_copy(test_path("slides", "basic-wide.pdf"), tmpdir, overwrite = TRUE)
 
     withr::local_dir(tmpdir)
 
     quiet_cli(
-        build_html("slides.Rmd", "widescreen.html", rmd_args = list(
-            output_options = list(nature = list(ratio = "16:9"))
-        ))
-    )
-
-    quiet_cli(
         build_pptx(
-            "widescreen.html",
+            "basic-wide.pdf",
             "widescreen.pptx",
             slides = 1:2,
             complex_slides = TRUE,
-            keep_intermediates = TRUE
+            keep_intermediates = FALSE
         )
     )
 
     expect_true(fs::file_exists("widescreen.pptx"))
-    expect_true(fs::file_exists("widescreen.pdf"))
-    expect_true(fs::file_exists("widescreen.html"))
 
     expect_equal(
         nrow(officer::pptx_summary(officer::read_pptx("widescreen.pptx"))),
