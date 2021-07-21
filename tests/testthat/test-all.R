6a9e066e66b200285113425774e6f47fc6d75f55
Garrick Aden-Buie <garrick@adenbuie.com>
2021-07-20 23:27:32 -0400

    Update `build_all()` to use `path_from()` and add tests

---
+++ tests/testthat/test-all.R
@@ -0,0 +1,45 @@
+test_that("build_all() from basic slides.Rmd", {
+    skip_if_not_chrome_installed()
+    skip_if_not_installed("av")
+    skip_if_not_installed("officer")
+    skip_if_not_installed("webshot2")
+
+    tmpdir <- withr::local_tempdir()
+    fs::dir_copy(test_path("slides", "basic"), tmpdir, overwrite = TRUE)
+
+    withr::local_dir(tmpdir)
+
+    quiet_cli(
+        build_all("slides.Rmd", slides = NULL)
+    )
+
+    expect_true(fs::file_exists("slides.html"))
+    expect_true(fs::file_exists("slides.pdf"))
+    expect_true(fs::file_exists("slides.zip"))
+    expect_true(fs::file_exists("slides.gif"))
+    expect_true(fs::file_exists("slides.mp4"))
+    expect_true(fs::file_exists("slides.pptx"))
+})
+
+test_that("build_all() from basic slides.Rmd with a few excluded formats", {
+    skip_if_not_chrome_installed()
+    skip_if_not_installed("av")
+    skip_if_not_installed("officer")
+    skip_if_not_installed("webshot2")
+
+    tmpdir <- withr::local_tempdir()
+    fs::dir_copy(test_path("slides", "basic"), tmpdir, overwrite = TRUE)
+
+    withr::local_dir(tmpdir)
+
+    quiet_cli(
+        build_all("slides.Rmd", slides = NULL, exclude = c("html", "pdf", "png"))
+    )
+
+    expect_false(fs::file_exists("slides.html"))
+    expect_false(fs::file_exists("slides.pdf"))
+    expect_false(fs::file_exists("slides.zip"))
+    expect_true(fs::file_exists("slides.gif"))
+    expect_true(fs::file_exists("slides.mp4"))
+    expect_true(fs::file_exists("slides.pptx"))
+})
