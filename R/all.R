89f5ec9793a922c683a84c7012f857f000711121
Garrick Aden-Buie <garrick@adenbuie.com>
2022-06-08 17:25:56 -0400

    feat: Deprecate `build_all()`

--- R/all.R
+++ R/all.R
@@ -1,5 +1,8 @@
 #' Build xaringan slides to multiple outputs.
 #'
+#' @description
+#' `r lifecycle::badge("deprecated")`
+#'
 #' Build xaringan slides to multiple outputs. Options are `"html"`, `"social"`
 #' `"pdf"`, `"png"`, `"gif"`, `"mp4"`, and `"pptx"`. See each individual
 #' build_*() function for details about each output type.
@@ -37,6 +40,8 @@ build_all <- function(
     slides = "all",
     fps = 1
 ) {
+    lifecycle::deprecate_warn("0.1.0", "build_all()")
+
     # Check that input file has the correct extension
     assert_path_ext(input, "rmd")
 
@@ -76,54 +81,54 @@ build_all <- function(
     # Do each step in order to ensure updates propagate
     # (or we use the current version of the required build step)
     if ("social" %in% include) {
-        build_social(
-            input = input,
-            output_file = path_from(input, "social")
+        to_social(
+            from = input,
+            to = path_from(input, "social")
         )
         if (identical("social", include)) {
             return()
         }
     }
     if ("html" %in% include || req_html) {
-        build_html(input = input, output_file = step_html)
+        to_html(from = input, to = step_html)
     }
     if ("pdf" %in% include || req_pdf) {
-        build_pdf(
-            input = step_html,
-            output_file = step_pdf,
+        to_pdf(
+            from = step_html,
+            to = step_pdf,
             complex_slides = complex_slides,
             partial_slides = partial_slides,
             delay = delay
         )
     }
     if ("png" %in% include) {
-        build_png(
-            input = step_pdf,
-            output_file = path_from(input, "png"),
+        to_png(
+            from = step_pdf,
+            to = path_from(input, "png"),
             density = density,
             slides = slides
         )
     }
     if ("gif" %in% include) {
-        build_gif(
-            input = step_pdf,
-            output_file = path_from(input, "gif"),
+        to_gif(
+            from = step_pdf,
+            to = path_from(input, "gif"),
             density = density,
             fps = fps
         )
     }
     if ("mp4" %in% include) {
-        build_mp4(
-            input = step_pdf,
-            output_file = path_from(input, "mp4"),
+        to_mp4(
+            from = step_pdf,
+            to = path_from(input, "mp4"),
             density = density,
             fps = fps
         )
     }
     if ("pptx" %in% include) {
-        build_pptx(
-            input = step_pdf,
-            output_file = path_from(input, "pptx"),
+        to_pptx(
+            from = step_pdf,
+            to = path_from(input, "pptx"),
             density = density
         )
     }
