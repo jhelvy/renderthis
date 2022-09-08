ef55519b007cb3231ee4d31c2b97a27dcc5a8aab
Garrick Aden-Buie <garrick@adenbuie.com>
2022-09-07 23:34:00 -0400

    social example is interactive() only

--- man/examples/examples_social.R
+++ man/examples/examples_social.R
@@ -1,5 +1,7 @@
-with_example("slides.Rmd", requires_chrome = TRUE, requires_packages = "webshot2", {
-    # Render png image of first slide from Rmd file
-    # sized for sharing on social media
-    to_social("slides.Rmd")
-})
+if (interactive()) {
+    with_example("slides.Rmd", requires_chrome = TRUE, requires_packages = "webshot2", {
+        # Render png image of first slide from Rmd file
+        # sized for sharing on social media
+        to_social("slides.Rmd")
+    })
+}
